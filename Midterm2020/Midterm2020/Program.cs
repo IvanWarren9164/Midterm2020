47150036593103102d2f7cc7e334f4c3721d4543
Mike C <Michch333@gmail.com>
2020-11-25 18:43:49 -0500

    blakes changes

--- Midterm2020/Midterm2020/Program.cs
+++ Midterm2020/Midterm2020/Program.cs
@@ -9,108 +9,142 @@ namespace Midterm2020
         static void Main(string[] args)
         {
             var testListOfBooks = new List<Book>();
-            testListOfBooks.Add(new Book("Visible Man", "Eli Ralphson", DateTime.Now, Status.CheckedOut));
-            /*testListOfBooks.Add(new Book("Harry Potter and the chamber of secrets", "JK Rowling"));
+            testListOfBooks.Add(new Book("Harry Potter and the chamber of secrets", "JK Rowling"));
             testListOfBooks.Add(new Book("Sisterhood of the traveling pants", "Joe Schmo"));
             testListOfBooks.Add(new Book("To Kill A Mockingbird", "Harper Lee"));
             testListOfBooks.Add(new Book("The Great Gatsby", "F Scott Fitzgerald"));
             testListOfBooks.Add(new Book("A Passage to India", "EM Foster"));
             testListOfBooks.Add(new Book("Invisible Man", "Ralph Ellison"));
+            testListOfBooks.Add(new Book("Visible Man", "Eli Ralphson", DateTime.Now));
             testListOfBooks.Add(new Book("Beloved", "Toni Morrison"));
-            testListOfBooks.Add(new Book("Deloved", "Toni Morrison"));*/
+            testListOfBooks.Add(new Book("Deloved", "Toni Morrison"));
 
             Library.DisplayAllBooks(testListOfBooks);
 
-            Library.CheckOutBook(testListOfBooks, testListOfBooks[0]);
-        }
-    }
+            string mySearchString = Console.ReadLine();
 
-    public enum Status 
-    {
-        OnShelf = 0,
-        CheckedOut = 1
-    }
+            SearchForBook(testListOfBooks, mySearchString);
+        }
 
-    public abstract class Library 
-    {
-        public static void DisplayAllBooks(List<Book> listOfBooks)
+        private static void SearchForBook(List<Book> testListOfBooks, string mySearchString)
         {
-            foreach (Book book in listOfBooks)
+            Console.WriteLine("Do you want to search by name or author?");
+
+            foreach (Book currentBook in testListOfBooks)
             {
-                Console.WriteLine($"Title: {book.Title}");
-                Console.WriteLine($"Author: {book.Author}");
-                DynamicDueDate(book);
-                Console.WriteLine("\n\n");
-            }
-        }
-        public static void DynamicDueDate(Book book)
-        {
-            if (book.Status == Status.CheckedOut)
-            {
-                Console.WriteLine("Status: Checked Out");
-                Console.WriteLine($"Due Date: {book.DueDate}");
-            }
-            else
-            {
-                Console.WriteLine("Status: Available");
-                Console.WriteLine("No Due Date");
-            }
-        }
-        public static void ReturnBook(List<Book> listOfBooks, Book bookToBeReturned)
-        {
-            foreach (Book book in listOfBooks)
-            {
-                if (book.Title.Equals(bookToBeReturned.Title, StringComparison.OrdinalIgnoreCase))
+                if (mySearchString == "name")
                 {
-                    book.Status = Status.OnShelf;
-                    book.DueDate = DateTime.MinValue;
+                    Console.WriteLine(currentBook.Title);
+                }
+                else if (mySearchString == "author")
+                {
+                    Console.WriteLine(currentBook.Title);
+
+                }
+
+
+            }
+        }
+
+        public enum Status
+        {
+            OnShelf = 0,
+            CheckedOut = 1
+        }
+
+        public abstract class Library
+        {
+            public static void DisplayAllBooks(List<Book> listOfBooks)
+            {
+                foreach (Book book in listOfBooks)
+                {
+                    Console.WriteLine($"Title: {book.Title}");
+                    Console.WriteLine($"Author: {book.Author}");
+                    DynamicDueDate(book);
+                    Console.WriteLine("\n\n");
                 }
             }
-        }
-        public static void CheckOutBook(List<Book> listOfBooks, Book bookToBeCheckedOut)
-        {
-            foreach (Book book in listOfBooks)
+
+
+
+
+
+            public static void DynamicDueDate(Book book)
             {
-                if (book.Title.Equals(bookToBeCheckedOut.Title, StringComparison.OrdinalIgnoreCase) && book.Status == Status.OnShelf)
+
+
+
+
+
+
+                if (book.Status == Status.CheckedOut)
                 {
-                    book.Status = Status.CheckedOut;
-                    book.DueDate = DateTime.Now.AddDays(14);
-                    Console.WriteLine("You have checked out: " + book.Title + "\nIt is due: " + DateTime.Now.AddDays(14));
+                    Console.WriteLine("Status: Checked Out");
+                    Console.WriteLine($"Due Date: {book.DueDate}");
                 }
                 else
                 {
-                    Console.WriteLine(book.Title + " is already checked out, please select a different book \n");
+                    Console.WriteLine("Status: Available");
+                    Console.WriteLine("No Due Date");
                 }
             }
+            public static void ReturnBook(List<Book> listOfBooks, Book bookToBeReturned)
+            {
+                foreach (Book book in listOfBooks)
+                {
+                    if (book.Title.Equals(bookToBeReturned.Title, StringComparison.OrdinalIgnoreCase))
+                    {
+                        book.Status = Status.OnShelf;
+                        book.DueDate = DateTime.MinValue;
+                    }
+                }
+            }
+            public static void CheckOutBook(List<Book> listOfBooks, Book bookToBeCheckedOut)
+            {
+                foreach (Book book in listOfBooks)
+                {
+                    if (book.Title.Equals(bookToBeCheckedOut.Title, StringComparison.OrdinalIgnoreCase))
+                    {
+                        book.Status = Status.CheckedOut;
+                        book.DueDate = DateTime.Now.AddDays(14);
+                    }
+
+
+                }
+            }
+            public static void CreateLibrary()
+            {
+                // depends on how our streamwriter / reader works. We want to create new books based on this txt file
+            }
         }
-        public static void CreateLibrary() 
+
+
+
+        public class Book
         {
-            // depends on how our streamwriter / reader works. We want to create new books based on this txt file
+            public Book(string title, string author)
+            {
+                Title = title;
+                Author = author;
+                Status = Status.OnShelf;
+                DueDate = DateTime.MinValue;
+            }
+            public Book(string title, string author, DateTime dueDate)
+            {
+                Title = title;
+                Author = author;
+                Status = Status.CheckedOut;
+                DueDate = dueDate;
+            }
+            public string Title { get; set; }
+            public string Author { get; set; }
+            public Status Status { get; set; }
+            public DateTime DueDate { get; set; }
+
+
+
+
         }
+
+
     }
-    public class Book
-    {
-        /*public Book(string title, string author)
-        {
-            Title = title;
-            Author = author;
-            Status = Status.OnShelf;
-            DueDate = DateTime.MinValue;
-        }*/
-        public Book(string title, string author, DateTime dueDate, Status status)
-        {
-            Title = title;
-            Author = author;
-            Status = status;
-            DueDate = dueDate;
-        }
-        public string Title { get; set; }
-        public string Author { get; set; }
-        public Status Status { get; set; }
-        public DateTime DueDate { get; set; }
-
-
-
-
-    }
-}
