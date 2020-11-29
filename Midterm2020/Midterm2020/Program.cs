9c9a4b8fb548e38894f777f8baed086a77f0e566
darshanpatel5758 <darshanpatel5758@gmail.com>
2020-11-28 21:06:57 -0500

    returnAndCheckOut

--- Midterm2020/Midterm2020/Program.cs
+++ Midterm2020/Midterm2020/Program.cs
@@ -18,32 +18,114 @@ namespace Midterm2020
             testListOfBooks.Add(new Book("The Great Gatsby", "F Scott Fitzgerald"));
             testListOfBooks.Add(new Book("A Passage to India", "EM Foster"));
             testListOfBooks.Add(new Book("Invisible Man", "Ralph Ellison"));
-            testListOfBooks.Add(new Book("Visible Man", "Eli Ralphson", DateTime.Now));
+            testListOfBooks.Add(new Book("Visible Man", "Eli Ralphson"));
             testListOfBooks.Add(new Book("Beloved", "Toni Morrison"));
             testListOfBooks.Add(new Book("Deloved", "Toni Morrison"));
 
+            // Library.DisplayAllBooks(testListOfBooks);
 
+           
 
-            Library.DisplayAllBooks(testListOfBooks);
+            Library.CreateList(testListOfBooks);
+            Book.ReadLibrary();
 
-            var listOfTitle = new List<string>();
-
-            if (!File.Exists(Global.path))
+            do
             {
-                for (int i = 0; i < testListOfBooks.Count; i++)
+                Console.WriteLine("Select one of the option below : ");
+                Console.WriteLine("1. Search for a book by author.");
+                Console.WriteLine("2. Search for a book by title keyword.");
+                Console.WriteLine("3. CheckOut Book ");
+                Console.WriteLine("4. Return Book");
+                Console.WriteLine("5. Exit ");
+                int select = Convert.ToInt32(Console.ReadLine());
+
+
+                if (select == 1)
                 {
-                    // Create a file to write to.
-                    listOfTitle.Add(testListOfBooks[i].Title);
-                    listOfTitle.Add(testListOfBooks[i].Author);
+                    Console.WriteLine("Please enter name of the auther : ");
+                    string autherOfTheBook = Console.ReadLine();
+                    Library.SearchAuther(autherOfTheBook);
+                    Console.WriteLine("Please select the option below : ");
+                    Console.WriteLine("1. Check Out Book : ");
+                    Console.WriteLine("2. Return a book");
+                    Console.WriteLine("3. Return to Main Menu : ");
 
+                    int select1 = Convert.ToInt32(Console.ReadLine());
+                    if (select1 == 1)
+                    {
+                        Library.CheckOutBook1(testListOfBooks, autherOfTheBook);
+                        Library.CreateList(testListOfBooks);
+                        Book.ReadLibrary();
+                        break;
+                    }
+                    else if (select1 == 2)
+                    {
+                        Library.ReturnBook(testListOfBooks, autherOfTheBook);
+                        Library.CreateList(testListOfBooks);
+                        Book.ReadLibrary();
+                        break;
 
+                    }
                 }
+                else if (select == 2 )
+                {
+                    Console.WriteLine("Please enter title of the book : ");
+                    string titleOfTheBook = Console.ReadLine();
+                    Library.Searchtitle(titleOfTheBook);
+                    Console.WriteLine("Please select the option below : ");
+                    Console.WriteLine("1. Check Out Book : ");
+                    Console.WriteLine("2. Return a book");
+                    Console.WriteLine("3. Return to Main Menu : ");
+                    int select1 = Convert.ToInt32(Console.ReadLine());
+
+                    if (select1 == 1)
+                    {
+                        Library.CheckOutBook1(testListOfBooks, titleOfTheBook);
+                        Library.CreateList(testListOfBooks);
+                        Book.ReadLibrary();
+                        break;
+                    }
+                    else if (select1 == 2)
+                    {
+                        Library.ReturnBook(testListOfBooks, titleOfTheBook);
+                        Library.CreateList(testListOfBooks);
+                        Book.ReadLibrary();
+                        break;
+
+                    }
+                    
+                }
+                else if (select == 3)
+                {
+                    Console.WriteLine("Please enter name of the auther or title of the book");
+                    string checkout = Console.ReadLine();
+                    Library.CheckOutBook1(testListOfBooks, checkout);
+                    Library.CreateList(testListOfBooks);
+                    Book.ReadLibrary();
+                    break;
+                }
+                else if (select == 4)
+                {
+                    Console.WriteLine("Please enter name of the auther or title of the book");
+                    string checkout = Console.ReadLine();
+                    Library.ReturnBook(testListOfBooks, checkout);
+                    Library.CreateList(testListOfBooks);
+                    Book.ReadLibrary();
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("\nBye!");
+                    break;
+                }
+                
+               
+                
 
 
+            } while (true);
+           
 
-            }
-
-            Book.CreateLibrary();
         }
     }
 
@@ -56,28 +138,30 @@ namespace Midterm2020
 
     static class Global // Create a path variable.
     {
-        public static string path = Path.Combine(Environment.CurrentDirectory, @"\Midterm2020\Midterm2020\Demo.txt");
+        public static string path = Path.Combine(Environment.CurrentDirectory, @"\Midterm2020\Midterm2020\MyText.txt");
        
     }
 
     public abstract class Library
     {
+        /*
         public static void DisplayAllBooks(List<Book> listOfBooks)
         {
+         
             foreach (Book book in listOfBooks)
             {
                 Console.WriteLine($"Title: {book.Title}");
                 Console.WriteLine($"Author: {book.Author}");
                 DynamicDueDate(book);
                 Console.WriteLine("\n\n");
-            }
-        }
+            } 
+            
 
 
+           
+        }*/
 
-
-
-        public static void DynamicDueDate(Book book)
+      /*  public static void DynamicDueDate(Book book)
         {
             if (book.Status == Status.CheckedOut)
             {
@@ -89,19 +173,89 @@ namespace Midterm2020
                 Console.WriteLine("Status: Available");
                 Console.WriteLine("No Due Date");
             }
+        }*/ 
+       
+        public static void SearchAuther(string name)
+        {
+            int num = 1;
+            char space = ' ';
+            string[] readText = File.ReadAllLines(Global.path);
+            Console.WriteLine("\n\n");
+            Console.WriteLine("No. Title 				          Author Name		                     	Status 		                                    Due Date");
+            Console.WriteLine("=====================================================================================================================================================================");
+
+            for (int i = 0; i < File.ReadAllLines(Global.path).Length; i++)
+            {
+                i++;
+                if (readText[i].Equals(name, StringComparison.OrdinalIgnoreCase))
+                { 
+                     string line1 = "";
+                          for (int j = i-1; j < i + 3; j++)
+                          {
+                                line1 = line1 + readText[j].PadRight(45, space) + " ";
+                          }
+                     Console.WriteLine($"{num}.  {line1} ");
+                    num++;
+                    
+                }
+                
+                
+                
+            }
+            Console.WriteLine("\n\n");
+            
         }
-        public static void ReturnBook(List<Book> listOfBooks, Book bookToBeReturned)
+
+        public static void Searchtitle(string name)
+        {
+            int num = 1;
+            char space = ' ';
+            string[] readText = File.ReadAllLines(Global.path);
+            Console.WriteLine("\n\n");
+            Console.WriteLine("No. Title 				          Author Name		                     	Status 		                                    Due Date");
+            Console.WriteLine("=====================================================================================================================================================================");
+
+            for (int i = 0; i < File.ReadAllLines(Global.path).Length; i++)
+            {
+                if (readText[i].Equals(name, StringComparison.OrdinalIgnoreCase))
+                {
+                    string line1 = "";
+                    for (int j = i; j < i + 4; j++)
+                    {
+                        line1 = line1 + readText[j].PadRight(45, space) + " ";
+                    }
+                    Console.WriteLine($"{num}.  {line1} ");
+                    num++;
+                    
+                }
+                
+            }
+            Console.WriteLine("\n\n");
+        }
+
+        public static void ReturnBook(List<Book> listOfBooks, string bookToBeReturned)
         {
             foreach (Book book in listOfBooks)
             {
-                if (book.Title.Equals(bookToBeReturned.Title, StringComparison.OrdinalIgnoreCase))
+                if (book.Title.Equals(bookToBeReturned, StringComparison.OrdinalIgnoreCase))
                 {
                     book.Status = Status.OnShelf;
                     book.DueDate = DateTime.MinValue;
+                    Console.WriteLine("Return Done!");
+                }
+                if (book.Author.Equals(bookToBeReturned, StringComparison.OrdinalIgnoreCase))
+                {
+                    book.Status = Status.OnShelf;
+                    book.DueDate = DateTime.MinValue;
+                    Console.WriteLine("Return Done!");
                 }
             }
+            if (File.Exists(Global.path))
+                File.Delete(Global.path);
         }
-        public static void CheckOutBook(List<Book> listOfBooks, Book bookToBeCheckedOut)
+
+    
+       /* public static void CheckOutBook(List<Book> listOfBooks, Book bookToBeCheckedOut)
         {
             foreach (Book book in listOfBooks)
             {
@@ -112,13 +266,89 @@ namespace Midterm2020
                 }
 
             }
-        }
-        public static void Create()
+        }*/
+        public static void CheckOutBook1(List<Book> listOfBooks, string bookToBeCheckedOut)
         {
+            
+            foreach (Book book in listOfBooks)
+            {
+                if (book.Title.Equals(bookToBeCheckedOut, StringComparison.OrdinalIgnoreCase) )
+                {
+                        book.Status = Status.CheckedOut;
+                        book.DueDate = DateTime.Now.AddDays(14);
+                        Console.WriteLine("Checkout Done!");                     
+                                   
+                    
+                }
+                if (book.Author.Equals(bookToBeCheckedOut, StringComparison.OrdinalIgnoreCase))
+                {
+                    book.Status = Status.CheckedOut;
+                    book.DueDate = DateTime.Now.AddDays(14);
+                    Console.WriteLine("Checkout Done!");
+
+
+                }
+                /*
+                else if (book.Title.Equals(bookToBeCheckedOut, StringComparison.OrdinalIgnoreCase) && book.Status.Equals("CheckedOut"))
+                {
+                    Console.WriteLine("Book is already checked out! Please select athoer book.");
+                    break;
+                }
+                */
+            }
+
+
+
+            /*
+             string[] readText = File.ReadAllLines(Global.path);
+
+
+             for (int i = 0; i < File.ReadAllLines(Global.path).Length; i+=4)
+             {
+                 if (readText[i].Equals(bookToBeCheckedOut, StringComparison.OrdinalIgnoreCase))
+                 {
+
+                     for (int j = i + 2; j == i; j++)
+                     {
+                         readText[j] = "CheckedOut";
+                     }
+                     for (int j = i+3 ; j == i; j++)
+                     {
+                         readText[j] = DateTime.Now.AddDays(14).ToString();
+                     }
+
+
+
+                 }
+
+             }*/
+            if (File.Exists(Global.path))
+                File.Delete(Global.path);
+
+
+        }
+
+        public static void CreateList(List<Book> testListOfBooks)
+        {
+            var listOfTitle = new List<string>();
+            for (int i = 0; i < testListOfBooks.Count; i++)
+            {
+                listOfTitle.Add(testListOfBooks[i].Title);
+                listOfTitle.Add(testListOfBooks[i].Author);
+                listOfTitle.Add(testListOfBooks[i].Status.ToString());
+                listOfTitle.Add(testListOfBooks[i].DueDate.ToString());
+
+            }
+            
+            if (!File.Exists(Global.path))
+                File.WriteAllLines(Global.path, listOfTitle);
+          
             // depends on how our streamwriter / reader works. We want to create new books based on this txt file
         }
+
+       
     }
-    public class Book
+    public class Book 
     {
         public Book(string title, string author)
         {
@@ -140,15 +370,32 @@ namespace Midterm2020
         public DateTime DueDate { get; set; }
 
 
-        public static void CreateLibrary()
+        public static void ReadLibrary()
         {
-            
+            int num = 1;
+            char space = ' ';
             string[] readText = File.ReadAllLines(Global.path);
-            foreach (string s in readText)
+            Console.WriteLine("\n\n");
+            Console.WriteLine("No. Title 				          Author Name		                     	Status 		                                    Due Date");
+            Console.WriteLine("=====================================================================================================================================================================");
+
+            for (int i = 0; i < File.ReadAllLines(Global.path).Length; i=i+4)
             {
-                Console.WriteLine(s);
+                string line1 = "";
+                for (int j = i; j < i+4 ; j++)
+                {
+                    line1 = line1 + readText[j].PadRight(45,space) + " ";
+                }
+                Console.WriteLine($"{num}.  {line1} ");
+                num++;
             }
+            Console.WriteLine("\n\n");
+
+
+           
             // depends on how our streamwriter / reader works. We want to create new books based on this txt file
         }
+
+        
     }
 }
